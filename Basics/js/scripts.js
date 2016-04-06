4bb1edbcaa1335845a7bdf16913d2fe95bfda1f6
Cthulh <tryglavson@hotmail.co.uk>
2016-04-06 22:20:36 +0100

    task 2.6

--- Basics/js/scripts.js
+++ Basics/js/scripts.js
@@ -1,3 +1,45 @@
+// *************** facebook button
+(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+
+$("#submitter").on("click", function() {
+      console.log("clicked");
+      return false;
+      // no code here!
+	});
+
 $(document).ready(function(){
-	alert("Document is ready!");
+// *************** smooth scrolling
+	var $root = $('html, body');
+      $('.navbar-nav a').click(function() {
+        var href = $.attr(this, 'href');
+        $root.animate({
+            scrollTop: $(href).offset().top
+        }, 500, function () {
+            window.location.hash = href;
+        });
+        return false;
+     });
+// *************** tooltip
+     $(function () {
+     	$('#tooltip1').tooltip();
+     });
+// *************** tooltip
+     $(function () {
+     	$('[data-toggle="tooltip"]').tooltip();
+     });
+// *************** stellar
+     $('#contactme').stellar();
+
+     $("#submitter").on("click", function() {
+      console.log("clicked");
+      return false;
+      // no code here!
+	});
+
 });
