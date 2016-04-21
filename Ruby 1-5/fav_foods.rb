15d767a607feb9a9e14e2851ff8e9a17cf6ede88
Cthulh <tryglavson@hotmail.co.uk>
2016-04-21 10:49:42 +0100

    task 3.3 arrays

---
+++ Ruby 1-5/fav_foods.rb
@@ -0,0 +1,12 @@
+def fav_foods
+	food_array = []
+	3.times do
+		puts "Name a favourite food."
+		food_array<<gets.chomp
+	end
+	p food_array
+	puts "Your favourite foods are #{food_array.join(", ")}."
+	food_array.each { |food| puts "I like #{food} too!"}
+end
+
+fav_foods
